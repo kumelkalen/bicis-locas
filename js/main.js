25fee7068de39ead0e2459d5e2d484e796739156
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-04 15:33:03 -0400

    Tomamos los valores de nuestros input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var nombre = document.getElementById("name").value;
+	var apellido = document.getElementById("lastname").value;
+	var email = document.getElementById("input-email").value;
+	var contraseña = document.getElementById("input-password").value;
+	var twitter = document.getElementById("input-social").value;
 }
