63ca2d0c965de22ab832ee4ae8a4801887b6d9a2
kumelkalen <free.your.mind.fc@gmail.com>
2017-08-04 15:58:52 -0400

    Tomamos los valores de todos los input

--- js/main.js
+++ js/main.js
@@ -3,5 +3,9 @@ function validateForm(){
 	var apellido = document.getElementById("lastname").value;
 	var email = document.getElementById("input-email").value;
 	var contraseña = document.getElementById("input-password").value;
+	var tagBici = document.getElementsByTagName("select");
+	var bici = tagBici[0].value;
 	var twitter = document.getElementById("input-social").value;
+	var check = document.getElementById("check");
+	
 }
